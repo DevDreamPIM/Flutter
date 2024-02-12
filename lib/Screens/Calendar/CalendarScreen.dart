17373d25009976e95a4f3bf2bd1281c0597cb43c
Marwen-Hammami <hammamimarwen.d@gmail.com>
2024-02-12 23:38:52 +0100

    Calendar UI

--- lib/Screens/Calendar/CalendarScreen.dart
+++ lib/Screens/Calendar/CalendarScreen.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:epilepto_guard/consts.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:table_calendar/table_calendar.dart';
 import 'package:weather/weather.dart';
 
 import '../../colors.dart';
@@ -20,6 +21,13 @@ class _CalendarScreenState extends State<CalendarScreen> {
   Weather? _weather;
   String cityName = "Tunis";
 
+  DateTime today = DateTime.now();
+  void _onDaySelected(DateTime day, DateTime focusedDay) {
+    setState(() {
+      today = day;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
@@ -34,14 +42,101 @@ class _CalendarScreenState extends State<CalendarScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/background/background.png'),
-            fit: BoxFit.cover,
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/background/background.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              children: [
+                _buildWeatherUI(),
+                _buildCalendarUI(),
+              ],
+            ),
           ),
+          Positioned(
+            bottom: 16,
+            right: 16,
+            child: ElevatedButton(
+              onPressed: () {
+                _showSelectionPopup(context);
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.purple,
+              ),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 16.0),
+                child: Text('Add',
+                    style: TextStyle(fontSize: 18.0, color: Colors.white)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showSelectionPopup(BuildContext context) {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return ListView(
+          shrinkWrap: true,
+          children: [
+            ListTile(
+              title: Center(child: Text('Add Drug')),
+              onTap: () {
+                // Handle selection of Item 1
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Center(child: Text('Add Drug Reminder')),
+              onTap: () {
+                // Handle selection of Item 2
+                Navigator.pop(context);
+              },
+            ),
+            // ListTile(
+            //   title: Center(child: Text('Item 3')),
+            //   onTap: () {
+            //     // Handle selection of Item 3
+            //     Navigator.pop(context);
+            //   },
+            // ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildCalendarUI() {
+    return Padding(
+      padding: const EdgeInsets.all(6.0),
+      child: Container(
+        margin: EdgeInsets.only(top: 20),
+        child: Column(
+          children: [
+            Container(
+              child: TableCalendar(
+                rowHeight: 80,
+                headerStyle: HeaderStyle(
+                    formatButtonVisible: false, titleCentered: true),
+                availableGestures: AvailableGestures.all,
+                selectedDayPredicate: (day) => isSameDay(day, today),
+                focusedDay: today,
+                firstDay: DateTime.utc(2023),
+                lastDay: DateTime.utc(2034),
+                onDaySelected: _onDaySelected,
+              ),
+            ),
+            Text("Selected Day: ${today.toString().split(" ")[0]}"),
+          ],
         ),
-        child: _buildWeatherUI(),
       ),
     );
   }
@@ -131,7 +226,7 @@ class _CalendarScreenState extends State<CalendarScreen> {
                                   ],
                                 ),
                               ),
-                              SizedBox(width: 8),
+                              const SizedBox(width: 8),
                               Text(
                                 "${_weather?.temperature?.celsius?.toStringAsFixed(0)}°C",
                                 style: const TextStyle(fontSize: 28),
@@ -144,11 +239,11 @@ class _CalendarScreenState extends State<CalendarScreen> {
                           // ),
                           Text(
                             "Wind: ${_weather?.windSpeed?.toStringAsFixed(0)}m/s",
-                            style: TextStyle(fontSize: 16),
+                            style: const TextStyle(fontSize: 16),
                           ),
                           Text(
                             "Humidity: ${_weather?.humidity?.toStringAsFixed(0)}%",
-                            style: TextStyle(fontSize: 16),
+                            style: const TextStyle(fontSize: 16),
                           ),
                         ],
                       ),
