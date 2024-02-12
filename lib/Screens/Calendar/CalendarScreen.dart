45078649f1367b7327cc2df17e118630c37531e6
Marwen-Hammami <hammamimarwen.d@gmail.com>
2024-02-12 22:46:38 +0100

    Weather Widget  with API Data

--- lib/Screens/Calendar/CalendarScreen.dart
+++ lib/Screens/Calendar/CalendarScreen.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:epilepto_guard/consts.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:weather/weather.dart';
 
 import '../../colors.dart';
@@ -36,25 +37,27 @@ class _CalendarScreenState extends State<CalendarScreen> {
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/images/background/splash_screen.png'),
+            image: AssetImage('assets/images/background/background.png'),
             fit: BoxFit.cover,
           ),
         ),
-        child: _buildUI(),
+        child: _buildWeatherUI(),
       ),
     );
   }
 
-  Widget _buildUI() {
+  Widget _buildWeatherUI() {
     if (_weather == null) {
       return const Center(
         child: CircularProgressIndicator(),
       );
     } else {
+      DateTime now = _weather!.date!;
+
       return Align(
         alignment: Alignment.topCenter,
         child: Container(
-          margin: EdgeInsets.only(top: 20),
+          margin: const EdgeInsets.only(top: 20),
           width: MediaQuery.of(context).size.width * 0.9,
           height: MediaQuery.of(context).size.height * 0.2,
           child: ClipRect(
@@ -62,13 +65,12 @@ class _CalendarScreenState extends State<CalendarScreen> {
               filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
               child: Container(
                 width: MediaQuery.of(context).size.width * 0.9,
-                height: MediaQuery.of(context).size.height * 0.2,
+                height: MediaQuery.of(context).size.height * 0.1,
                 decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 175, 80, 212).withOpacity(0.5),
+                  color: AppColors.purple.withOpacity(0.5),
                   borderRadius: BorderRadius.circular(10),
                 ),
-                padding: EdgeInsets.symmetric(
-                    horizontal: 20), // Adding left and right padding
+                padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: Row(
                   children: [
                     // First Column
@@ -78,13 +80,14 @@ class _CalendarScreenState extends State<CalendarScreen> {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Text(
-                            'Monday',
-                            style: TextStyle(fontSize: 16), // Normal size
+                            DateFormat("EEEE, MMMM dd").format(now),
+                            style: const TextStyle(fontSize: 16),
                           ),
                           Text(
-                            '8:00 AM', // Buffer size
-                            style: TextStyle(
-                                fontSize: 24, fontWeight: FontWeight.bold),
+                            DateFormat("hh:mm a").format(now),
+                            style: const TextStyle(
+                              fontSize: 28,
+                            ),
                           ),
                         ],
                       ),
@@ -97,27 +100,54 @@ class _CalendarScreenState extends State<CalendarScreen> {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Text(
-                            'Town Name',
-                            style: TextStyle(fontSize: 16),
+                            _weather?.areaName ?? "No Area Name",
+                            style: const TextStyle(fontSize: 16),
                           ),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.end,
                             children: [
-                              Image.asset(
-                                'assets/images/background/drug.png',
-                                width: 50,
-                                height: 50,
+                              Container(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 4),
+                                decoration: BoxDecoration(
+                                  color: AppColors.purple.withOpacity(0.2),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Column(
+                                  children: [
+                                    Image.network(
+                                      "http://openweathermap.org/img/wn/${_weather?.weatherIcon}@4x.png",
+                                      width: 50,
+                                      height: 50,
+                                    ),
+                                    Text(
+                                      _weather?.weatherDescription ??
+                                          "No Description",
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w400,
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
                               SizedBox(width: 8),
                               Text(
-                                '23',
-                                style: TextStyle(
-                                    fontSize: 24, fontWeight: FontWeight.bold),
+                                "${_weather?.temperature?.celsius?.toStringAsFixed(0)}°C",
+                                style: const TextStyle(fontSize: 28),
                               ),
                             ],
-                          ), // Adjust the image path
+                          ),
+                          // Text(
+                          //   "Max: ${_weather?.tempMax?.celsius?.toStringAsFixed(0)}°C - Min: ${_weather?.tempMin?.celsius?.toStringAsFixed(0)}°C",
+                          //   style: TextStyle(fontSize: 16),
+                          // ),
                           Text(
-                            'Max: 25°C - Min: 15°C',
+                            "Wind: ${_weather?.windSpeed?.toStringAsFixed(0)}m/s",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          Text(
+                            "Humidity: ${_weather?.humidity?.toStringAsFixed(0)}%",
                             style: TextStyle(fontSize: 16),
                           ),
                         ],
