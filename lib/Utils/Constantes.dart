a0c61007a33249b0729d70fccb9446695559cb0a
Marwen-Hammami <hammamimarwen.d@gmail.com>
2024-02-22 13:51:34 +0100

    Corrected Bluetooth permission overflow

--- lib/Utils/Constantes.dart
+++ lib/Utils/Constantes.dart
@@ -1,4 +1,4 @@
-class Constantes{
-  static const String URL_API = "http://localhost:9090";
+class Constantes {
+  static const String URL_API = "http://172.16.4.134:9090";
   static const String URL_API_USER = "/users";
 }
