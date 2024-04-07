4d48d0d198fa2c36360c95508a9ebb5029077672
jouhayna <cheikh.jouhayna@esprit.tn>
2024-04-07 14:44:15 +0100

    aidpull

--- lib/Utils/Constantes.dart
+++ lib/Utils/Constantes.dart
@@ -1,6 +1,6 @@
 class Constantes {
   //static const String URL_API = "https://backend-epileptoguard.onrender.com";
-  static const String URL_API = "http://127.0.0.1:9090";
+  static const String URL_API = "http://192.168.1.17:9090";
   static const String URL_API_USER = "/users";
   static const String USER_IMAGE_URL = "http://127.0.0.1:9090/img/user";
 //static const String USER_IMAGE_URL ="https://backend-epileptoguard.onrender.com/img/user";
